e07fc57dafe04de9016b4469d91db81e6223d2cb
Robertoscuro <rober-alga@outlook.com>
2020-07-16 13:58:48 +0200

    La placa ya lee los tres sensores, y manuales añadidos

--- Monitorizacion/Monitorizacion.ino
+++ Monitorizacion/Monitorizacion.ino
@@ -1,9 +1,19 @@
 #include "WiFi.h"
+#include "DHT.h"
  
-const char* ssid = ""; //introduce the credentials
-const char* password =  "";
- 
+const char* ssid = "ONOA2F9"; //introduce the credentials
+const char* password =  "hCNYeM3RbGK2";
+int lightPin = 23; 
+int temperaturePin = 22; 
+int detectionPin = 1; 
+int temp;
+int hum;
+DHT dht(temperaturePin, DHT11);
 void setup() {
+  pinMode(lightPin, INPUT);
+  pinMode(temperaturePin, INPUT);
+  pinMode(detectionPin, INPUT);
+  dht.begin();
   Serial.begin(115200);
   WiFi.begin(ssid, password);
  
@@ -16,4 +26,24 @@ void setup() {
  
 }
  
-void loop() {}
+void loop() {
+  if(digitalRead(lightPin)){
+    Serial.println("Sin luz");
+  }else{
+    Serial.println("Con luz");
+  }
+  
+  hum= dht.readHumidity();
+  temp= dht.readTemperature();
+  Serial.print("Humedad: ");
+   Serial.println(hum);
+  Serial.print("Temperatura: ");
+   Serial.println(temp);
+    if(digitalRead(detectionPin)==HIGH){
+    Serial.println("Movimiento");
+  }else{
+    Serial.println("Sin movimiento");
+  }
+  delay(1000);
+
+  }
